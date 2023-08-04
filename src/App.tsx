f882371ee7f042ad2ace4e788e651974638e517f
Vivaan Singhvi <s945958@student.knoxschools.org>
2023-08-04 00:12:32 -0400

    testing the main route too

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import React from "react";
 import Projects from "./components/Projects";
 import { Route, Routes, HashRouter } from "react-router-dom";
 
+function Hello() {
+  return <p>Hello world!</p>
+}
+
 function App() {
   return (
     <>
       <HashRouter>
         <Routes>
+          <Route path="/" element={<Hello />} />
           <Route path="/projects" element={<Projects />} />
         </Routes>
       </HashRouter>

3558529bd067aae6dfe2c80e6e7f86317e102cf9
Vivaan Singhvi <vivaansinghvi8@gmail.com>
2023-08-13 00:57:01 -0400

    added background animation

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,16 @@ import Header from "./components/Header";
 import Projects from "./components/Projects";
 import AboutMe from "./components/AboutMe";
 import { Route, Routes } from "react-router-dom";
-
-function Hello() {
-  return <p>Hello world!</p>;
-}
+import Background from "./components/Background";
+import "./App.css"
 
 function App() {
   return (
     <>
+      <Background />
       <Header />
       <Routes>
-        <Route path="/" element={<Hello />} />
+        <Route path="/" element={<div>Hello</div>} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/aboutme" element={<AboutMe />} />
       </Routes>
---
+++ src/components/Background.tsx
@@ -0,0 +1,57 @@
+import "./Background.css";
+import { useEffect, useState } from "react";
+
+function randInt(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function Cloud() {
+  return (
+    <>
+      <img
+        className="Background-cloud"
+        src={`/images/clouds/cloud${randInt(0, 12)}.png`}
+        style={{
+          top: `${randInt(-10, 90)}vh`,
+          opacity: `${randInt(50, 100)}%`,
+          animation: `${randInt(15, 30)}s ltr-slide forwards linear`,
+        }}
+      />
+    </>
+  );
+}
+
+export default function Background() {
+  const [cloudListOne, updateCloudListOne] = useState([<Cloud />]);
+  const [cloudListTwo, updateCloudListTwo] = useState(new Array<JSX.Element>());
+  const [onListOne, setOnListOne] = useState(true);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (onListOne) {
+        updateCloudListOne([...cloudListOne, <Cloud />]);
+        if (cloudListOne.length === 10) {
+          updateCloudListTwo(new Array<JSX.Element>());
+          setOnListOne(false);
+        }
+      } else {
+        updateCloudListTwo([...cloudListTwo, <Cloud />]);
+        if (cloudListTwo.length === 10) {
+          updateCloudListOne(new Array<JSX.Element>());
+          setOnListOne(true);
+        }
+      }
+    }, randInt(3000, 5000));
+
+    return () => clearInterval(interval);
+  });
+
+  return (
+    <>
+      <div className="Background">
+        {cloudListOne}
+        {cloudListTwo}
+      </div>
+    </>
+  );
+}
